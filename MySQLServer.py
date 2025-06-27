a41e5047e3f3e681d0f5ff179d521b6a5573ccc3
Beamlak12Dereje <beamlakdereje19@gmail.com>
2025-06-27 15:29:39 +0300

    Add Python script to create alx_book_store database if not exists

---
+++ MySQLServer.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+def create_database():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Bami@2012"
+        )
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    create_database()
