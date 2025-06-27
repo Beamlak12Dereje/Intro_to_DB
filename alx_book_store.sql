cb1ade55d7a2310493c325c94322a2bc32d60be4
Beamlak12Dereje <150372095+Beamlak12Dereje@users.noreply.github.com>
2025-06-27 15:21:53 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE Order_Details (
     orderdetailsid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantitative DOUBLE,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
